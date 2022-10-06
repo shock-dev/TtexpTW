79ecf81f8d3ad88330d7812e68ec0b18d590bd51
shock-dev <shock.work@mail.ru>
2022-10-06 15:59:41 +0300

    prepare template for api

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,48 @@
 <template>
   <div id="app">
     <div class="header">
-      <button class="btn">Загрузить данные</button>
+      <button v-if="!loading" class="btn" :disabled="loading">
+        Загрузить данные
+      </button>
+      <Loader v-else />
+    </div>
+    <div v-if="list.length" class="list">
+      <User
+        v-for="i in list"
+        :id="i.id"
+        :key="i.id"
+        :list="i.list"
+        :success="i.success"
+        :fails="i.fails"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import User from "@/components/User";
+import Loader from "@/components/Loader";
+
 export default {
   name: "App",
-  components: {},
+  data() {
+    return {
+      list: [
+        { id: 1, success: 18, fails: 2, list: ["test", "test"] },
+        { id: 2, success: 18, fails: 2, list: ["test", "test"] },
+        { id: 3, success: 18, fails: 2, list: ["test", "test"] },
+      ],
+      loading: false,
+    };
+  },
+  components: { Loader, User },
+  methods: {
+    getUsers() {
+      if (!this.loading) {
+        //
+      }
+    },
+  },
 };
 </script>
 
@@ -24,12 +57,33 @@ export default {
   padding: 0 5px;
 }
 
+p {
+  margin: 0;
+}
+
+.header {
+  display: flex;
+  margin-bottom: 20px;
+}
+
 .btn {
   background: #7979fb;
   border-radius: 3px;
   border: 0;
   color: #fff;
-  padding: 10px 20px;
+  padding: 20px 30px;
   cursor: pointer;
+  margin-right: 20px;
+
+  &:disabled {
+    background: #b1b1b1;
+    cursor: not-allowed;
+  }
+}
+
+.list {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
 }
 </style>
---
+++ src/components/User.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wrapper">
+    <header class="header">
+      <p>User {{ id }}</p>
+      <p>
+        <span class="success">{{ success }}</span>
+        /
+        <span class="fail">{{ fails }}</span>
+      </p>
+    </header>
+    <ul class="titles">
+      <li v-for="(i, index) in list" :key="index">{{ i }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "User",
+  props: {
+    id: Number,
+    success: Number,
+    fails: Number,
+    list: Array,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  border: 1px solid black;
+  border-radius: 3px;
+  padding: 10px;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.success {
+  color: green;
+}
+
+.fail {
+  color: red;
+}
+
+.titles {
+  margin: 15px 0 0 0;
+}
+
+.titles > *:not(:last-child) {
+  margin-bottom: 5px;
+}
+</style>
